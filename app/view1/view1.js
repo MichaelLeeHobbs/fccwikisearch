1446f9aa0691ce5eae015a38d8d5cb78fababe3a
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-07-13 13:58:10 +0900

    update controller to use wikiSvc

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,18 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl',
+            resolve: {
+                results: ['wikiSvc', function (wikiSvc) {
+                    return wikiSvc.query('test');
+                }]
+            }
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
-
-}]);
+    .controller('View1Ctrl', ['$scope', 'results', function ($scope, results) {
+        $scope.results = results.data.query.pages;
+    }]);
