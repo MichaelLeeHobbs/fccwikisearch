981bd1d2fcf4a711454c6fcc3392763c66f7f6ef
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-07-13 13:56:15 +0900

    fixed service to use jsonp

--- app/services.js
+++ app/services.js
@@ -5,8 +5,8 @@ angular.module('myApp.services', [])
         return {
             query: function (search) {
                 var promise = $http({
-                    method: 'GET',
-                    url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + search,
+                    method: 'JSONP',
+                    url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + search + '&callback=JSON_CALLBACK',
                     cache: true
                 });
                 promise.success(function (data, status, headers, conf) {
